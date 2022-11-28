9d5970fc77c2673f9866200331f9c9df07f5d478
Levon Maksudyan <levonmak7@gmail.com>
2022-11-28 22:42:45 +0500

    full

--- main/main.js
+++ main/main.js
@@ -5,28 +5,40 @@ let formElement = document.querySelector(".pop_up");
 let nameInput = document.querySelector("#EditName");
 // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector("#EditJob");
+let profileButton = document.querySelector(".profile__edit") 
+let popUpClose = document.querySelector(".pop_up__close")
  // Воспользуйтесь инструментом .querySelector()
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
+function openPupUp (evt){
+    formElement.style.display = "block";
+}
+
+function closePupUp (evt) {
+    formElement.style.display = "none";
+}
+
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    // let hello = "Привет";                       // О том, как это делать, расскажем позже.
     let nameTitle = document.querySelector(".profile__title");
-    nameTitle.textContent = "Привет";
-    nameInput.value = 
-    
+    nameTitle.textContent = nameInput.value;
+    // nameInput.value = nameTitle;
+    let jobsubtitle = document.querySelector(".profile__subtitle");
+    jobsubtitle.textContent = jobInput.value;
     // Получите значение полей jobInput и nameInput из свойства value
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
     console.log(nameTitle);
-    console.log(nameInput .value)
+    console.log(nameInput.value)
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+profileButton.addEventListener('click', openPupUp);
+popUpClose.addEventListener('click', closePupUp);
 
-    
